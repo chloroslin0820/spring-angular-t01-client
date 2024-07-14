126684eb81c48ced59d97498be336dd221b64279
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-14 19:30:12 +0800

    config env

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 const API_URL = 
   environment.API_DEV_URL || 
