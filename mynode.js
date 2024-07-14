b785c7a5ab5aa70812ff01b88518a4b60387e992
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-14 20:23:18 +0800

    fix package setting

--- mynode.js
+++ mynode.js
@@ -4,6 +4,8 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
+// dotenv.config({ path: path.join(__dirname, 'src/.env') });
+
 const envFile = `export const environment = {
     API_URL: '${process.env.API_URL}',
     API_DEV_URL: '${process.env.API_DEV_URL}',
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: '',
-    API_DEV_URL: '',
+    API_URL: 'https://chloroslin-spring-angular-t01-server.onrender.com/',
+    API_DEV_URL: 'http://www.localhost:8080/',
 };
